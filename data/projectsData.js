d64c63f0594dfedb1b091aae53bc8a260a727b20
cheng-hsin <s950091qq@gmail.com>
2023-01-07 11:59:20 +0800

    add project2

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,17 +1,16 @@
 const projectsData = [
   {
-    title: 'Movie Ticket Booking App',
-    description: `A Movie Ticket Booking App. It is a full-stack application built with Next.js, Prisma, and PostgreSQL.`,
+    title: 'CineMarmot',
+    description: `A Movie Ticket Booking App. It is a full-stack application built with Next.js,
+     Prisma, and PostgreSQL.`,
     imgSrc: '/static/images/cinemarmot.png',
     href: 'https://gitlab.com/have-a-nice-build/cinemarmot',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'Game project with Python',
+    description: `The project includes tic-tac-toe and word guessing game. It is made for TV show to.`,
+    imgSrc: '/static/images/gameproject.png',
+    href: 'https://github.com/cheng-hsin/tvprogramme_pygame',
   },
 ]
 
